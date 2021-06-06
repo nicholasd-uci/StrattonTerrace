2020a75825005a7eea44924fcf877158696211bb
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-06 13:45:21 -0700

    done for the day

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Home.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' 
+
+import Picture from '../../Assets/Pictures/Picture.jpg'
 
 
 function Home() {
     return (
         <div className="home">
-     <h1>content</h1>
-        </div>
+        <div className="home__mainPicture">
+       <img src={Picture} alt="main_image"/>
+       </div>
+
+    </div>
     )
 }
 
