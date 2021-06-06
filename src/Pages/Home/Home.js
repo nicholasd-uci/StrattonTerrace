352435525f96fdf49924777d6cb1a5ba5fe5b637
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-06 13:56:28 -0700

    bug fix

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import './Home.css'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' 
-
-import Picture from '../../Assets/Pictures/Picture.jpg'
+// import Picture from '../../Assets/Pictures/Picture.jpg'
 
 
 function Home() {
     return (
         <div className="home">
-        <div className="home__mainPicture">
+        {/* <div className="home__mainPicture">
        <img src={Picture} alt="main_image"/>
-       </div>
+       </div> */}
 
     </div>
     )
