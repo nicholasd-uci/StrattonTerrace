1e6c9560f9591c191f0c223bea5bfbec3f012f79
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-06 14:05:36 -0700

    picture update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Home.css'
-// import Picture from '../../Assets/Pictures/Picture.jpg'
+import Picture from '../../Assets/Pictures/Picture1.jpg'
 
 
 function Home() {
     return (
         <div className="home">
-        {/* <div className="home__mainPicture">
+        <div className="home__mainPicture">
        <img src={Picture} alt="main_image"/>
-       </div> */}
+       </div>
 
     </div>
     )
