b0e0bce152c3d452b4ccd430357da1cec3062400
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-14 11:11:49 -0700

    marketing

--- src/Pages/Solutions/Solutions.js
+++ src/Pages/Solutions/Solutions.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap';
 import './Solutions.css'
+import IMG12 from '../../Assets/Images/IMG12.jpg'
 
 
 function Solutions() {
 
     return (
-        <div className="Solutions"> 
+      <div className="Solutions">
+        <div className="Solutions__background"> 
         <Container>
        <Row>
         <Col xs="6">.col-6</Col>
-        <Col xs="6">.col-6</Col>
+        <Col xs="6">
+        <img id="IMG12" src={IMG12} alt="Digital marketing"/>
+        </Col>
       </Row>
       </Container>
+      </div>
       </div>
     )
 }
