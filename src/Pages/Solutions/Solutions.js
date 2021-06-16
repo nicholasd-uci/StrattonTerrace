cb4e379a33acd5c74f49ec454aa9a5b128dd9c02
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-16 01:22:41 -0700

    solutions page about done

--- src/Pages/Solutions/Solutions.js
+++ src/Pages/Solutions/Solutions.js
@@ -14,28 +14,66 @@ function Solutions() {
     return (
       <div className="Solutions">
         <div className="Solutions__background"> 
-
-
+        <h1 className="Solutions__h1"> 
+        SOLUTIONS
+        </h1>
+        <p className="Solutions__p1">We provide full stack digital marketing solutions.</p>
        <Row>
         <Col id="Solutions__R1__col1" xl="4">
       <img className="Solutions__icons" src={website} alt="website"/>
+        <h3 className="Solutions__h3">
+          Website Design
+        </h3>
+        <p>
+        In today’s digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user’s first impression relates to web design. It’s also why web design services can have an immense impact on your company’s bottom line. 
+        </p>
         </Col>
         <Col id="Solutions__R1__col2" xl="4">
         <img className="Solutions__icons" src={Analytics} alt="Analytics"/>
+        <h3 className="Solutions__h3">
+          Analytics
+        </h3>
+        <p>
+        Google Analytics and supplementary solutions help you collect tons of information. Our experts help you install such tools, measure and interpret all the touchpoints and get actionable insights from the mess.
+        </p>
         </Col>
         <Col id="Solutions__R1__col3" xl="4">
         <img className="Solutions__icons" src={Email} alt="Email"/>
+        <h3 className="Solutions__h3">
+        Email Marketing
+        </h3>
+        <p>
+        Email is one of the cheapest and most effective online marketing tools. We help you grow and monetize your database enables you to retarget your prospects, profile your customers, create lookalike audiences.
+        </p>
         </Col>
       </Row>
       <Row>
         <Col id="Solutions__R2__col1" xl="4">
         <img className="Solutions__icons" src={Integration} alt="Integration"/>
+        <h3 className="Solutions__h3">
+        Integration
+        </h3>
+        <p>
+        Forget last click attribution. You need a holistic approach to your offline and online marketing channels. Some tend to convert better, whereas other raise awareness. We help you achieve your business goals with the right channels.
+        </p>
         </Col>
         <Col id="Solutions__R2__col2" xl="4">
         <img className="Solutions__icons" src={Optimization} alt="Optimization"/>
+        <h3 className="Solutions__h3">
+        Conversion Rate Optimization
+         </h3>
+         <p>
+         Bringing prospects is the easy part. Converting them is the hard one. We help you optimise your website towards your business goals, thus reducing your cost per acquisition.
+        </p>
         </Col>
         <Col id="Solutions__R2__col3" xl="4">
         <img className="Solutions__icons" src={Seo} alt="Seo"/>
+        <h3 className="Solutions__h3">
+        Content Marketing & SEO
+        </h3>
+        <p>
+        From content creation to technical, On-Page and Off-Page SEO, we can help you generate free by default traffic, which can increase your brand recognition and supplement your paid advertising efforts.
+        </p>
         </Col>
       </Row>
    
