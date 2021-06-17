1e868badb0b4c7e12df4e57a3de6260875dcc34c
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-17 00:35:55 -0700

    done for the night

--- src/Components/PricingTables/PricingTables.js
+++ src/Components/PricingTables/PricingTables.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './PricingTables.css'
 import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
+import { Container } from 'reactstrap';
 
 
 
@@ -13,8 +14,9 @@ import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
         render() {
         
           return (
+            <Container>
       <PricingTable  highlightColor='rgb(216, 188, 32)'>
-          <PricingSlot  onClick={this.submit} buttonText='ORDER NOW' title='Basic Web Package' priceText='$249/month'>
+          <PricingSlot  onClick={this.submit} buttonText='ORDER' title='Basic Web Package' priceText='$249/month'>
               <PricingDetail> <b>2</b> Stock Images</PricingDetail>
               <PricingDetail> <b>3</b> Page Website</PricingDetail>
               <PricingDetail> <b>1</b> jQuery Slider Banner</PricingDetail>
@@ -23,7 +25,7 @@ import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
               <PricingDetail>Complete Deployment</PricingDetail>
               <PricingDetail strikethrough> <b>Mobile Responsive</b></PricingDetail>
           </PricingSlot>
-          <PricingSlot highlighted onClick={this.submit} buttonText='SIGN UP' title='Startup Web Package' priceText='$349/month'>
+          <PricingSlot highlighted onClick={this.submit} buttonText='ORDER' title='Startup Web Package' priceText='$349/month'>
           <PricingDetail> <b>5</b> Stock Photos</PricingDetail>
               <PricingDetail> <b>5</b> Page Website</PricingDetail>
               <PricingDetail> <b>3</b> Banner Design</PricingDetail>
@@ -32,7 +34,7 @@ import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
               <PricingDetail>Complete Deployment</PricingDetail>
               <PricingDetail>Mobile Responsive will be Additional $200*</PricingDetail>
           </PricingSlot>
-          <PricingSlot  onClick={this.submit} buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month'>
+          <PricingSlot  onClick={this.submit} buttonText='ORDER' title='PROFESSIONAL' priceText='$99/month'>
           <PricingDetail> <b>10</b> Page Website</PricingDetail>
               <PricingDetail>CMS / Admin Panel Support</PricingDetail>
               <PricingDetail> <b>8</b> Stock Images</PricingDetail>
@@ -43,7 +45,7 @@ import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
           </PricingSlot>
       
       </PricingTable>
-
+      </Container>
 
           );
           
