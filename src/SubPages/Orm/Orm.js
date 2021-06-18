2d350469fec701d017ece31d4c03400eeab6c0da
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-18 12:31:47 -0700

    seo page uploaded

--- src/SubPages/Orm/Orm.js
+++ src/SubPages/Orm/Orm.js
@@ -4,32 +4,30 @@ import { Row, Col } from 'reactstrap';
 import ORM1 from '../../Assets/Images/ORM1.jpg'
 import ORM2 from '../../Assets/Images/ORM2.jpg'
 
-
 const styles = {
     image: {
         boxShadow: "5px 5px 20px -5px black",
         borderRadius: "25px",
-
     
     },
     }
 
 function Orm() {
     return (
-        <div className="Nonprofit">
-        <div className="Nonprofit__background"> 
+        <div className="Orm">
+        <div className="Orm__background"> 
         <Row>
-        <Col id="Nonprofit__R1__col2" xl="6">
+        <Col id="Orm__R1__col2" xl="6">
        <img className="img-fluid mb-xl-5" src={ORM2} alt="Digital" style={styles.image} />
         </Col>
-        <Col id="Nonprofit__R1__col1" xl="6">
-        <h3 className="Nonprofit__h3">
+        <Col id="Orm__R1__col1" xl="6">
+        <h3 className="Orm__h3">
         Online Reputation Management
         </h3>
-        <h4 className="Nonprofit__h4">
+        <h4 className="Orm__h4">
         What Is Online Reputation Management?
         </h4>
-        <p className="Nonprofit__p">
+        <p className="Orm__p">
         Whether your business is large or small — with one location or many — your customers are talking about you online. And the viral nature of today’s information can create or destroy your reputation in minutes.
 
 A solid online reputation also has tremendous impact on your bottom line. Recent research by Reputation.com found that online-sourced sales for businesses who have ORM programs in place grew 24% faster than for those not actively engaged in ORM activities.
@@ -41,16 +39,14 @@ By engaging in essential ORM activities — managing online reviews, maintaining
         </Col>
       </Row>
         <Row>
-        <Col id="Nonprofit__R1__col1" xl="6">
-       
-
-          <h3 className="Nonprofit__h3">
+        <Col id="Orm__R1__col1" xl="6">
+        <h3 className="Orm__h3">
         ORM
         </h3>
-       <p className="Nonprofit__p">
+       <p className="Orm__p">
         Stratton Terrace Marketing company will explain how ORM works and provides tips and strategies for getting started with an ORM program in your organization. You’ll learn:
         </p>
-       <ul className='Nonprofit__ul'>
+       <ul className='Orm__ul'>
         <li>
         What is Online Reputation Management?
         </li>
@@ -62,7 +58,7 @@ By engaging in essential ORM activities — managing online reviews, maintaining
         </li>
        </ul>
          </Col>
-        <Col id="Nonprofit__R1__col2" xl="4">
+        <Col id="Orm__R1__col2" xl="4">
        <img className="img-fluid mb-xl-5" src={ORM1} alt="Digital Advertising"  style={styles.image} />
         </Col>
       </Row>
--- src/SubPages/Seo/Seo.js
+++ src/SubPages/Seo/Seo.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import './Seo.css'
+import { Row, Col } from 'reactstrap';
+import Seo1 from '../../Assets/Images/Seo1.jpg'
+import Seo2 from '../../Assets/Images/Seo2.jpg'
+
+const styles = {
+    image: {
+        boxShadow: "5px 5px 20px -5px black",
+        borderRadius: "25px",
+    
+    },
+    }
 
 function SEO() {
     return (
-        <div>
-            
+        <div className="SEO">
+        <div className="SEO__background"> 
+        <Row>
+        <Col id="SEO__R1__col2" xl="6">
+       <img className="img-fluid mb-xl-5" src={Seo1} alt="Digital" style={styles.image} />
+        </Col>
+        <Col id="SEO__R1__col1" xl="6">
+        <h3 className="SEO__h3">
+        Search Engine Optimization
+        </h3>
+        <h4 className="SEO__h4">
+        What Is Search Engine Optimization?
+        </h4>
+        <p className="SEO__p">
+        Search Engine Optimization is highly needed skill to have when marketing on the Google Search Engine. We have an array of ideas that bring highly qualified web search (Google, bing, yahoo) 
+        and social media traffic. Implementing SEO tools for your business will develop organic lead generation. It may be more sales for your e-commerce, a new client for your Law office or another 
+        patient at your dentistry. By the way.. What is SEO? Search Engine Optimization (according to wikipedia) is the process of affecting the visibility of a website or a web page in a web search 
+        engines’ unpaid results—often referred to as “natural”, “organic”, or “earned” results. In general, the earlier (or higher ranked on the search results page), and more frequently a site appears 
+        in the search results list, the more visitors it will receive from the search engine’s users; these visitors can then be converted into customers.
+        </p>
+        </Col>
+      </Row>
+        <Row>
+        <Col id="SEO__R1__col1" xl="6">
+        <h3 className="SEO__h3">
+        SEO
+        </h3>
+       <p className="SEO__p">
+        Stratton Terrace Marketing company will ensure that customers will receive:
+        </p>
+       <ul className='SEO__ul'>
+        <li>
+        Quality of traffic. You can attract all the visitors in the world, but if they're coming to your site because Google tells them you're a resource for Apple computers when really you're a farmer selling apples, that is not quality traffic. Instead, you want to attract visitors who are genuinely interested in the products that you offer.
+
+        </li>
+        <li>
+        Quantity of traffic. Once you have the right people clicking through from those search engine results pages (SERPs), more traffic is better.
+
+        </li>
+        <li>
+        Organic results. Ads make up a significant portion of many SERPs. Organic traffic is any traffic that you don't have to pay for.
+
+        </li>
+       </ul>
+         </Col>
+        <Col id="SEO__R1__col2" xl="4">
+       <img className="img-fluid mb-xl-5" src={Seo2} alt="Digital Advertising"  style={styles.image} />
+        </Col>
+      </Row>
+     
+      </div>
         </div>
     )
 }
