4c17eab2d7a6f3854498459a131b45538c609f04
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-06-28 09:22:56 -0700

    removing unused code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,21 +2,14 @@ import React, { useState } from 'react';
 import './Header.css'
 import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink} from 'reactstrap';
 import Logo from '../../Assets/Logo/Logo.png'
-import TM from '../../Assets/Logo/Theme.jpg'
+
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <div className="header"
-             style={{
-      backgroundImage: `url(${TM})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-    }}
-        >
+        <div className="header">
           
 <Navbar light expand="md">
 <NavLink id="header__home" href="/"><img src={Logo} alt="Website"/></NavLink>
