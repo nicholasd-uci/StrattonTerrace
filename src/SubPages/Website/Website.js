77c06000277c279ae3b31e917ad1c665d6ad254e
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-07-07 14:27:55 -0700

    added pricingtables3.js & updated website,js files

--- src/SubPages/Website/Website.js
+++ src/SubPages/Website/Website.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './Website.css'
 import PricingTables from '../../Components/PricingTables/PricingTables'
 import PricingTables2 from '../../Components/PricingTables/PricingTables2'
+import PricingTables3 from '../../Components/PricingTables/PricingTables3'
 
 const Website = (props) => {
     return (
         <div className="Website">
                   <div className="Website__background">
+        <PricingTables3 />
         <PricingTables />
         <PricingTables2 />
+        
           </div>
           </div>
     );
