c57658b78e3d88f7ad347e7f4fdcd4c7e24c6c12
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-07-07 13:36:35 -0700

    updated hosting.js - packages details

--- src/Components/Hosting_pricing/HostingPricing.js
+++ src/Components/Hosting_pricing/HostingPricing.js
@@ -17,29 +17,43 @@ import { Container } from 'reactstrap';
             <Container>
       <PricingTable  highlightColor='rgb(216, 188, 32)'>
           <PricingSlot highlighted onClick={this.submit} buttonText='ORDER' title='Starter Hosting service package' priceText='$29.99/Mo'>
-              <PricingDetail>Hosting</PricingDetail>
-              <PricingDetail> Maintenance</PricingDetail>
-              <PricingDetail> Consultation</PricingDetail>
+              <PricingDetail>Hosting Assistance</PricingDetail>
+              <PricingDetail>Domain Protection - SSL Certified</PricingDetail>
+              <PricingDetail>Domain Support</PricingDetail>
               <PricingDetail 
-              > <b>Ecommerce</b></PricingDetail>
-              <PricingDetail strikethrough> <b>Emails</b></PricingDetail>
+              > <b>Fast Page Loading</b></PricingDetail>
+              <PricingDetail 
+              > <b>Ecommerce Support</b></PricingDetail>
               <PricingDetail strikethrough> <b>Revisions</b></PricingDetail>
+              <PricingDetail strikethrough> <b>Email Marketing Template</b></PricingDetail>
           </PricingSlot>
-          <PricingSlot onClick={this.submit} buttonText='ORDER' title='Basic Hosting and Maintenance' priceText='$65.99/Mo'>
-          <PricingDetail>Hosting</PricingDetail>
-              <PricingDetail> Maintenance</PricingDetail>
-              <PricingDetail> Consultation</PricingDetail>
-              <PricingDetail> <b>Ecommerce</b></PricingDetail>
-              <PricingDetail> <b>Emails</b></PricingDetail>
-              <PricingDetail> <b>Revisions</b></PricingDetail>
+          <PricingSlot onClick={this.submit} buttonText='ORDER' title='Basic Hosting & Maintenance' priceText='$65.99/Mo'>
+          <PricingDetail>Hosting Assistance</PricingDetail>
+              <PricingDetail> Mo Maintenance Revisions</PricingDetail>
+              <PricingDetail> <b>Revisions: 1-2 </b></PricingDetail>
+              <PricingDetail> Monthly Consultation</PricingDetail>
+              <PricingDetail> <b>One: 1-Hour sessions </b></PricingDetail>
+              <PricingDetail> <b>Ecommerce Support</b></PricingDetail>
+              <PricingDetail> <b>Email Marketing Template</b></PricingDetail>
           </PricingSlot>
-          <PricingSlot highlighted onClick={this.submit} buttonText='ORDER' title='Advanced Hosting and Maintenance' priceText='$99.99/Mo'>
-          <PricingDetail>Hosting</PricingDetail>
-              <PricingDetail> Maintenance</PricingDetail>
-              <PricingDetail> Consultation</PricingDetail>
-              <PricingDetail> <b>Ecommerce</b></PricingDetail>
-              <PricingDetail> <b>Emails</b></PricingDetail>
-              <PricingDetail> <b>Revisions</b></PricingDetail>
+          <PricingSlot onClick={this.submit} buttonText='ORDER' title='Advanced Hosting & Maintenance' priceText='$77.99/Mo'>
+          <PricingDetail>Hosting Assistance</PricingDetail>
+              <PricingDetail> Mo Maintenance Revisions</PricingDetail>
+              <PricingDetail> <b>Revisions: 3-4 </b></PricingDetail>
+              <PricingDetail> Monthly Consultation</PricingDetail>
+              <PricingDetail> <b>Two: 1-Hour sessions </b></PricingDetail>
+              <PricingDetail> <b>Ecommerce Support</b></PricingDetail>
+              <PricingDetail> <b>Email Marketing Template</b></PricingDetail>
+          </PricingSlot>
+          <PricingSlot highlighted onClick={this.submit} buttonText='ORDER' title='Unlimited Hosting & Maintenance' priceText='$99.99/Mo'>
+          <PricingDetail>Hosting Assistance</PricingDetail>
+              <PricingDetail> Mo Maintenance Revisions</PricingDetail>
+              <PricingDetail> <b>Revisions: Unlimited</b></PricingDetail>
+              <PricingDetail> Monthly Consultation</PricingDetail>
+              <PricingDetail> <b>Three: 1-Hour sessions </b></PricingDetail>
+              <PricingDetail> <b>Ecommerce Support</b></PricingDetail>
+              <PricingDetail> <b>Email Marketing Template</b></PricingDetail>
+              
           </PricingSlot>
       </PricingTable>
       </Container>
