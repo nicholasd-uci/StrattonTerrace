bf62abbcf7d6fafb33e21676e58cdaedc0a6d85f
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-10-19 12:06:53 -0400

    photo upadte on About us

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './About.css'
 import AboutBanner from '../../Components/AboutBanner/AboutBanner'
 import Nicholas from '../../Assets/OurTeam/Nicholas.png'
-import Lawrence from '../../Assets/OurTeam/Lawrence. png'
+import Lawrence from '../../Assets/OurTeam/Lawrence.jpg'
+import Jack from '../../Assets/OurTeam/OBrien_Jack.png'
 import { MDBRow, MDBCol, MDBCard, MDBCardBody } from 'mdbreact'
 
 function About () {
@@ -66,6 +67,57 @@ function About () {
                 </a>
               </MDBCol>
             </MDBCol>
+            <MDBCol lg='6' md='12' className='mb-5'>
+            <MDBCol md='4' lg='6' className='float-left'>
+                <img
+                  src={Jack}
+                  className='mx-auto mb-md-0 mb-4 rounded z-depth-1 img-fluid'
+                  tag='img'
+                  alt='Sample avatar'
+                />
+              </MDBCol>
+              <MDBCol md='8' lg='6' className='float-right'>
+                <h4 className='font-weight-bold mb-3'>Jack O'Brien</h4>
+                <h6 className='font-weight-bold grey-text mb-3'>
+                Account Manager & Sales Rep
+                </h6>
+                <p className='About__p'>
+                Nicholas Dallas is the founder of STM LLC. However, he is also blessed to own a California-registered 501(c)3 non-profit - Huntington Beach Lacrosse Club. Nicholas holds a Global Business Management degree and a minor in Economics from Regis College in Weston, MA. He is also a Full Stack Web Developer and loves helping business owners achieve self-growth.                </p>
+                <a href='https://www.linkedin.com/in/nicholas-dallas-54b5b097/' target='_blank' rel="noreferrer"  className='p-2 fa-lg li-ic'>
+                <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a href='mailto:info@strattonterrace.com' target='_blank' rel="noreferrer"  className='p-2 fa-lg email-ic'>
+                <i className="far fa-envelope"></i>
+                </a>
+                </MDBCol>
+            </MDBCol>
+
+            <MDBCol lg='6' md='12' className='mb-5'>
+              <MDBCol md='4' lg='6' className='float-left'>
+                <img
+                  src={Lawrence}
+                  className='mx-auto mb-md-0 mb-4 rounded z-depth-1 img-fluid'
+                  tag='img'
+                  alt='Sample avatar'
+                />
+              </MDBCol>
+              <MDBCol md='8' lg='6' className='float-right'>
+                <h4 className='font-weight-bold mb-3'>Laurynas Juzva</h4>
+                <h6 className='font-weight-bold grey-text mb-3'>
+                  Full stack web developer
+                </h6>
+                <p className='About__p'>
+                Laurynas is a full-stack Web Developer. He has a Bachelor of Business administration, but his primary focus and inspiration are Web Development. He is driven, self-motivated, and constantly experimenting with new technologies and techniques. He is very passionate about Web Development and strives to better himself as a developer, and the development community as a whole.
+                </p>
+                <a href='https://www.linkedin.com/in/lawerence-j/' target='_blank' rel="noreferrer"  className='p-2 fa-lg li-ic'>
+                <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a href='mailto:laurynas@strattonterrace.com' target='_blank' rel="noreferrer"  className='p-2 fa-lg email-ic'>
+                <i class="far fa-envelope"></i>
+                </a>
+              </MDBCol>
+            </MDBCol>
+              </MDBRow>
             <MDBCol xl='4' lg='6' md='12' className='mb-5'>
               <h4 className='font-weight-bold mb-3'>Pat Downer</h4>
               <h6 className='font-weight-bold grey-text mb-3'>
@@ -78,12 +130,12 @@ function About () {
                 Coordinator & Brand Marketing Specialist
               </h6>
             </MDBCol>
-            <MDBCol xl='4' lg='6' md='12' className='mb-5'>
+            {/* <MDBCol xl='4' lg='6' md='12' className='mb-5'>
               <h4 className='font-weight-bold mb-3'>Jack O'Brien</h4>
               <h6 className='font-weight-bold grey-text mb-3'>
                 Account Manager & Sales Rep
               </h6>
-            </MDBCol>
+            </MDBCol> */}
             <MDBCol xl='4' lg='6' md='12' className='mb-5'>
               <h4 className='font-weight-bold mb-3'>Brendan Ryan</h4>
               <h6 className='font-weight-bold grey-text mb-3'>
@@ -108,7 +160,6 @@ function About () {
               Graphic Designer
               </h6>
             </MDBCol>
-          </MDBRow>
         </MDBCardBody>
       </MDBCard>
       <p className='About__pTag'>
