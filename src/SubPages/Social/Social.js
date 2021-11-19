d059ad79abd293ee0009e2280e803615ef58582b
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-11-18 16:11:54 -0800

    Social Media subpage Update

--- src/SubPages/Social/Social.js
+++ src/SubPages/Social/Social.js
@@ -1,50 +1,41 @@
 import React from 'react'
 import './Social.css'
 import { Row, Col } from 'reactstrap';
-import Social1 from '../../Assets/Images/Social1.jpg'
-import Social2 from '../../Assets/Images/Social2.jpg'
+import Social0 from '../../Assets/Images/Social-Header.png'
+import Social1 from '../../Assets/Images/Social1.png'
 import Services from '../../Components/Services/Services'
 
-const styles = {
-    image: {
-        boxShadow: "5px 5px 20px -5px black",
-        borderRadius: "5px",
-    
-    },
-    }
+
 function Social() {
     return (
-        <div className="Social">
-          <Services />
-        <div className="Social__background"> 
-        <Row>
-        <Col id="Social__R1__col2" xl="6">
-       <img className="img-fluid mb-xl-5" src={Social1} alt="Digital" style={styles.image} />
-        </Col>
-        <Col id="Social__R1__col1" xl="6">
-        <h3 className="Social__h3">
-        Social Media Strategy Implementation
-        </h3>
-        <h4 className="Social__h4">
-        Stratton Terrace Marketing
-        </h4>
-        <p className="Social__p">
-        consults our clients to have a better understanding on what social media is and how it can (or cannot) help your business is the key to utilizing this powerful public relations tool.  
-        I Win Marketing analyzes  your current social media situation and provide a complete analysis and detailed strategy for how your business can use social media.
+
+      <div className="Social1">
+      <div className = "Social__intro" >
+      <h1 className = "Social__h1">Social Media Strategy<br/> Implementation</h1>
+      <a href = "#What-is-Social">
+      <img id = "Social__img" src={Social0} alt="Social art"/> </a>
+      </div>
+      <div id = "What-is-Social">
+      </div>
+      <Services />
+      <Row className="Social__row1" >
+  <Col id="Social__R1__col1" xl="4">
+         <img id = "Social__img__body" src={Social1} alt="Digital Advertising"/>
+          </Col>
+          <Col>
+          <h4 className="Social__h4">
+          What Is Social Media Strategy Implementation?
+          </h4>
+          <p className="Social__p">
+          consults our clients to have a better understanding on what social media is and how it can (or cannot) help your business is the key to utilizing this powerful public relations tool.  
+          Stratton Terrace Marketing analyzes  your current social media situation and provide a complete analysis and detailed strategy for how your business can use social media.
         We make sure that your social media efforts are focused and engaged.  Entering a conversation with potential customers is a great opportunity to impact their decision making.  
         This service will make sure that the conversation about your business is always positive by using social media to shape the topics and support your goals.
-        </p>
-        </Col>
-      </Row>
-        <Row>
-        <Col id="Social__R1__col1" xl="6">
-        <h3 className="Social__h3">
-        Social Media Marketing and Management may be difficult for businesses to harness the power of social media.
-        </h3>
-       <p className="Social__p">
-       Here is the most enhanced package that we offer:
-        </p>
-       <ul className='Social__ul'>
+          </p>
+          <h4 className="Social__h4">
+          Social Media Marketing and Management may be difficult for businesses to harness the power of social media.
+          </h4>
+          <ul className='Social__ul'>
         <li>
         FREE Social Media Account/Profile Creation (If needed)
         </li>
@@ -91,14 +82,9 @@ function Social() {
         24/7 email support & Monitoring (Savings over $3k monthly)
         </li>
        </ul>
-         </Col>
-        <Col id="Social__R1__col2" xl="4">
-       <img className="img-fluid mb-xl-5" src={Social2} alt="Digital Advertising"  style={styles.image} />
         </Col>
-      </Row>
-     
-      </div>
-        </div>
+        </Row>
+          </div>
     )
 }
 
