889bdd4b2372777782baaaf6c73402c527869a6d
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-11-22 14:41:36 -0800

    ORM subpage content update

--- src/SubPages/Orm/Orm.js
+++ src/SubPages/Orm/Orm.js
@@ -25,11 +25,13 @@ function Orm() {
           What Is Online Reputation Management?
           </h4>
           <p className="Orm__p">
-          Whether your business is large or small — with one location or many — your customers are talking about you online. And the viral nature of today’s 
-          information can create or destroy your reputation in minutes. A solid online reputation also has tremendous impact on your bottom line. Recent research by Reputation.com 
-          found that online-sourced sales for businesses who have ORM programs in place grew 24% faster than for those not actively engaged in ORM activities.
-          By engaging in essential ORM activities — managing online reviews, maintaining accurate business listings, engaging with local communities and target audiences on social media,
-          conducting customer surveys and acting on feedback — you can help ensure your organization’s success in today’s digital business environment.
+          Online reviews and recommendations are becoming increasingly important in today’s market. Business owners need to take steps now, 
+          before their competitors do so! Recent research by Reputation Management identifies that 24% of sales growth is due solely from online-sourced 
+          interactions for businesses who have ORM programs in place—and this doesn't even include the other benefits such as customer service improvement or less 
+          time spent on marketing campaigns overall because they can focus where people spend most: online interacting with brands instead of them directly through physical storefront locations
+          Do you want be left behind while everyone else takes advantage?
+          There are a number of steps you can take to ensure your organization’s success in today's digital business environment. Engage with ORM activities including managing online reviews, 
+          maintaining accurate listings and engaging with target audiences on social media or conduct customer surveys for feedback from those who have been served by the company already!
 </p>
           <h4 className="Orm__h4">
           ORM
