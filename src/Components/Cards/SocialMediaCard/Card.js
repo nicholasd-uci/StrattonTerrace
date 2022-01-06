f1cb2eee77d6cc5eda4f924463437c47720c9800
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-01-06 15:29:00 -0800

    updating social media cards

--- src/Components/Cards/SocialMediaCard/Card.js
+++ src/Components/Cards/SocialMediaCard/Card.js
@@ -38,174 +38,53 @@ function Card() {
                 </div>
                     </div>
                     <div className="card">
-                <img src={Professional} alt="" className="card__img" /> 
+                <img src={SocialMedia} alt="" className="card__img" /> 
                 <div className="card__content">
                     <div className="card__data">
-                         <h1 className="card__title">Professional Landing Page</h1>
-                         <span className="card__price">$600</span>
+                         <h1 className="card__title">1 Channel Social Media</h1>
+                         <span className="card__price">$249</span>
                 <ul>
-                <li> <b>5 </b> Page Website</li>
-                <li>CMS / Admin Panel Support</li>
-                <li><b>8 </b> Stock Images</li>
-                <li>Made with WordPress</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
+                <span 
+                style={{
+                color: 'rgb(168, 148, 32)',
+              }}>Leverage STM and our team of content developers and writers. We are here to meet with you, understand your vision, and then build a roadmap for your audience. 
+              </span>
+                <li>One Social Media Channel</li>
+                <li>Two Post Per Week</li>
+                <li>Facebook Instagram</li>
+                <li>LinkedIn TikTok</li>
+                <li>YouTube Hulu & Peacock</li>
                 </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
+            <a href="https://checkout.square.site/merchant/MLZ8ZQY9BSZZC/checkout/A2QQNXOYG75LNSNAYIPAI66J" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
             <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
              </div>
                 </div>
                     </div>
                     <div className="card">
-                <img src={Athlete} alt="" className="card__img" /> 
+                <img src={SocialMedia} alt="" className="card__img" /> 
                 <div className="card__content">
                     <div className="card__data">
-                         <h1 className="card__title">Athlete Recruiting Page</h1>
-                         <span className="card__price">$2,000</span>
+                         <h1 className="card__title">1 Channel Social Media</h1>
+                         <span className="card__price">$249</span>
                 <ul>
-                <li><b>5 </b>Stock Photos</li>
-                <li><b>10+ </b>Page Website</li>
-                <li><b>3 </b>Banner Design</li>
-                <li>Made with WordPress</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
+                <span 
+                style={{
+                color: 'rgb(168, 148, 32)',
+              }}>Leverage STM and our team of content developers and writers. We are here to meet with you, understand your vision, and then build a roadmap for your audience. 
+              </span>
+                <li>One Social Media Channel</li>
+                <li>Two Post Per Week</li>
+                <li>Facebook Instagram</li>
+                <li>LinkedIn TikTok</li>
+                <li>YouTube Hulu & Peacock</li>
                 </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
+            <a href="https://checkout.square.site/merchant/MLZ8ZQY9BSZZC/checkout/A2QQNXOYG75LNSNAYIPAI66J" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
             <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
              </div>
                 </div>
                     </div>
                         </div>
-                        <div className="cards__container">
-            <div className="card">
-                <img src={Basic_Web} alt="" className="card__img" /> 
-
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Basic Web Pack</h1>
-                         <span className="card__price">$350</span>
-                <ul>
-                <li><b>2 </b> Stock Images</li>
-                <li><b>1 </b>Page Website</li>
-                <li><b>1 </b> Made with WIX</li>
-                <li><b>1 </b> Contact/Query Form</li>
-                <li>48 to 72 hours TAT</li>
-                <li>Complete Deployment</li>
-                <li><del><b>Mobile Responsive</b></del> </li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                    <div className="card">
-                <img src={Startup_web} alt="" className="card__img" /> 
-
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Startup Web Pack</h1>
-                         <span className="card__price">$1,350</span>
-                <ul>
-                <li> <b>5 </b> Stock Photos</li>
-                <li><b>3 </b> Page Website</li>
-                <li><b>3 </b> Banner Design</li>
-                <li>Made with WIX</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                    <div className="card">
-                <img src={Professional_Web} alt="" className="card__img" /> 
-
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Professional Web Pack</h1>
-                         <span className="card__price">$2,150</span>
-                <ul>
-                <li><b>5 </b>Page Website</li>
-                <li>CMS / Admin Panel Support</li>
-                <li><b>8 </b>Stock Images</li>
-                <li>Made with WIX</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                        </div>
-                        <div className="cards__container">
-            <div className="card">
-                <img src={Elite} alt="" className="card__img" /> 
-
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Elite Web Pack</h1>
-                         <span className="card__price">$450</span>
-                <ul>
-                <li><b>1 </b> Page Website</li>
-                <li><b>2 </b>Stock Images</li>
-                <li><b>1 </b> Made with WordPress</li>
-                <li><b>1 </b> Contact/Query Form</li>
-                <li>48 to 72 hours TAT</li>
-                <li>Complete Deployment</li>
-                <li><del><b>Mobile Responsive</b></del> </li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                    <div className="card">
-                <img src={Corporate} alt="" className="card__img" /> 
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Corporate Web pack</h1>
-                         <span className="card__price">$1,550</span>
-                <ul>
-                <li> <b>10 </b> Page Website</li>
-                <li>CMS / Admin Panel Support</li>
-                <li><b>8 </b> Stock Images</li>
-                <li>Made with WordPress</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                    <div className="card">
-                <img src={Business} alt="" className="card__img" /> 
-                <div className="card__content">
-                    <div className="card__data">
-                         <h1 className="card__title">Business Web Pack</h1>
-                         <span className="card__price">$2,450</span>
-                <ul>
-                <li><b>5 </b>Stock Photos</li>
-                <li><b>10+ </b>Page Website</li>
-                <li><b>3 </b>Banner Design</li>
-                <li>Made with WordPress</li>
-                <li>FREE Google Friendly Sitemap</li>
-                <li>Complete Deployment</li>
-                <li>Mobile Responsive will be Additional $200*</li>
-                </ul>
-            <a href="https://www.stmathlete.com/home" target="_blank" rel="noreferrer"  className="card__button_order">ORDER</a>
-            <a href="facetime:16578779300" target="_blank" rel="noreferrer"  className="card__button_call">CALL US</a>
-             </div>
-                </div>
-                    </div>
-                        </div>
+                     
                         </>
 
                         
